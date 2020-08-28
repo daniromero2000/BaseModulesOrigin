e3d4dcdfbb11dca99493f4f447e3f440d0aefaa6
Dalkyel <j.alejandrocr@gmail.com>
2020-08-28 08:12:17 -0500

    CamStudio Module

---
+++ CamStudio/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('camstudio::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('camstudio.name') !!}
+    </p>
+@endsection
