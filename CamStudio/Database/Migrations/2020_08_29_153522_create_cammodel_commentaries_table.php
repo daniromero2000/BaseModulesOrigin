1987aae950af4dde4cdd9a56f28f5ddcf94e3f05
Dalkyel <j.alejandrocr@gmail.com>
2020-08-29 10:43:48 -0500

    cammodel architecture

---
+++ CamStudio/Database/Migrations/2020_08_29_153522_create_cammodel_commentaries_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCammodelCommentariesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cammodel_commentaries', function (Blueprint $table) {
+            $table->id();
+            $table->string('commentary');
+            $table->string('user');
+            $table->integer('cammodel_id')->unsigned()->index();
+            $table->foreign('cammodel_id')->references('id')->on('cammodels')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cammodel_commentaries');
+    }
+}
