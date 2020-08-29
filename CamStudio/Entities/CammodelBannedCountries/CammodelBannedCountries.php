9eefc46064237e5fab08dc4b1af5f8fd89607e23
Dalkyel <j.alejandrocr@gmail.com>
2020-08-29 12:15:27 -0500

    cammodelbannedCountries architecture

--- CamStudio/Entities/CammodelBannedCountries/CammodelBannedCountries.php
+++ CamStudio/Entities/CammodelBannedCountries/CammodelBannedCountries.php
@@ -27,7 +27,6 @@ class CammodelBannedCountries extends Model
         'created_at',
         'updated_at',
         'deleted_at',
-        'status'
     ];
 
     protected $dates  = [
--- CamStudio/Entities/Cammodels/Cammodel.php
+++ CamStudio/Entities/Cammodels/Cammodel.php
@@ -39,7 +39,6 @@ class Cammodel extends Model
         'created_at',
         'updated_at',
         'deleted_at',
-        'status'
     ];
 
     protected $dates  = [
