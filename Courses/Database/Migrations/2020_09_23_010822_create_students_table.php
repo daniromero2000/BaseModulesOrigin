5bf9f431bccb000156efee230c17863fdfe00b55
Dalkyel <j.alejandrocr@gmail.com>
2020-09-22 20:40:23 -0500

    courses MVC

---
+++ Courses/Database/Migrations/2020_09_23_010822_create_students_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('cedula')->unique();
+            $table->string('name');
+            $table->string('last_name');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
