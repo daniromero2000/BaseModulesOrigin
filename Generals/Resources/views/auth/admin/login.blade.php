d99ba37493044f1d2474751628b69b8d00653d18
Daniel Romero <123romerod@gmail.com>
2020-09-22 21:13:05 -0500

    Home

--- Generals/Resources/views/auth/admin/login.blade.php
+++ Generals/Resources/views/auth/admin/login.blade.php
@@ -31,16 +31,7 @@
         <!-- Header -->
         <div class="header bg-gradient-primary py-5">
             <div class="container">
-                <div class="header-body text-center mb-7">
-                    <div class="row justify-content-center">
-                        <div class="col-xl-5 col-lg-6 col-md-8 px-5">
-                            <h1 class="text-white">Bienvenido a SmartCommerce!</h1>
-
-                            <p class="text-lead text-white">inicie sesión ingresando la información requerida a
-                                continuación</p>
-                        </div>
-                    </div>
-                </div>
+                @include('layouts.admin.welcome')
                 <div class="separator separator-bottom separator-skew zindex-100">
                     <svg x="0" y="0" viewBox="0 0 2560 100" preserveAspectRatio="none" version="1.1"
                         xmlns="http://www.w3.org/2000/svg">
