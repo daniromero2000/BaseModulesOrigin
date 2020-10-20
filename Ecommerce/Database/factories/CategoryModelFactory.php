b9bfc2f91b97d38959de4bdc911486cc4cf5234d
Dalkyel <j.alejandrocr@gmail.com>
2020-10-20 09:26:51 -0500

    Migration refactoring

--- Ecommerce/Database/factories/CategoryModelFactory.php
+++ Ecommerce/Database/factories/CategoryModelFactory.php
@@ -17,9 +17,9 @@ use Illuminate\Http\UploadedFile;
 
 $factory->define(Category::class, function (Faker\Generator $faker) {
     $name = $faker->unique()->randomElement([
-        'Niños',
-        'Niñas',
-        'Fisiológico'
+        'Recien Nacido',
+        'Baby',
+        'Mini'
     ]);
 
     $file = UploadedFile::fake()->image('category.png', 600, 600);
