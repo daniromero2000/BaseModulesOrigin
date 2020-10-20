58a3691da2f49b4980d3b0e675ceb1f92842f2ae
Dalkyel <j.alejandrocr@gmail.com>
2020-10-20 09:19:10 -0500

    cart fixed

--- Ecommerce/Database/Migrations/2020_10_19_232344_add_banner_to_categories_table.php
+++ Ecommerce/Database/Migrations/2020_10_19_232344_add_banner_to_categories_table.php
@@ -14,7 +14,7 @@ class AddBannerToCategoriesTable extends Migration
     public function up()
     {
         Schema::table('categories', function (Blueprint $table) {
-            $table->string('banner')->default('Sin Banner');
+            $table->string('banner')->default('Sin Banner')->after('cover');
         });
     }
 
