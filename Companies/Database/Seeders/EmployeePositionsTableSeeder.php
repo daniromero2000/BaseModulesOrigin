01f0ce341539c5416543be7ea6d201fccec21e5e
Dalkyel <j.alejandrocr@gmail.com>
2020-10-20 10:12:38 -0500

    Migration refactoring

--- Companies/Database/Seeders/EmployeePositionsTableSeeder.php
+++ Companies/Database/Seeders/EmployeePositionsTableSeeder.php
@@ -23,15 +23,15 @@ class EmployeePositionsTableSeeder extends Seeder
         ]);
 
         factory(EmployeePosition::class)->create([
-            'position' => 'Gerente',
-        ]);
-
-        factory(EmployeePosition::class)->create([
             'position' => 'Gerente Comercial',
         ]);
 
         factory(EmployeePosition::class)->create([
             'position' => 'Gerente Financiero',
+        ]);
+
+        factory(EmployeePosition::class)->create([
+            'position' => 'Gerente General',
         ]);
 
         factory(EmployeePosition::class)->create([
@@ -60,6 +60,54 @@ class EmployeePositionsTableSeeder extends Seeder
 
         factory(EmployeePosition::class)->create([
             'position' => 'Diseñador@',
+        ]);
+
+        factory(EmployeePosition::class)->create([
+            'position' => 'Jefe de Mantenimiento',
+        ]);
+
+        factory(EmployeePosition::class)->create([
+            'position' => 'Auxiliar de Mantenimiento',
+        ]);
+
+        factory(EmployeePosition::class)->create([
+            'position' => 'Auxiliar de Administrativa',
+        ]);
+
+        factory(EmployeePosition::class)->create([
+            'position' => 'Auxiliar Contable',
+        ]);
+
+        factory(EmployeePosition::class)->create([
+            'position' => 'Lider de Mercadeo',
+        ]);
+
+        factory(EmployeePosition::class)->create([
+            'position' => 'Fotograf@',
+        ]);
+
+        factory(EmployeePosition::class)->create([
+            'position' => 'Audiovisuales@',
+        ]);
+
+        factory(EmployeePosition::class)->create([
+            'position' => 'Lider de Desarrollo',
+        ]);
+
+        factory(EmployeePosition::class)->create([
+            'position' => 'Desarrollador',
+        ]);
+
+        factory(EmployeePosition::class)->create([
+            'position' => 'Gestor Operativo',
+        ]);
+
+        factory(EmployeePosition::class)->create([
+            'position' => 'Gestor Comercial',
+        ]);
+
+        factory(EmployeePosition::class)->create([
+            'position' => 'Administrador Sede',
         ]);
     }
 }
