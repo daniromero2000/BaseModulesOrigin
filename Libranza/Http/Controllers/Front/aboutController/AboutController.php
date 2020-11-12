836902f7dfe72fd54e5b0529cd2a0fcad617741f
Efrain Ignacio Espana Sanchez <efrain.espana@laso.local>
2020-11-12 14:10:39 -0500

    actualizacion modules

--- Libranza/Http/Controllers/Front/aboutController/AboutController.php
+++ Libranza/Http/Controllers/Front/aboutController/AboutController.php
@@ -2,12 +2,13 @@
 
 namespace Modules\Libranza\Http\Controllers\Front\aboutController;
 
+use Illuminate\Routing\Controller;
 use Illuminate\Http\Request;
 
 class AboutController extends Controller
 {
     public function index()
     {
-        return view('about::index');
+        return view('libranza.about.index');
     }        
 }
