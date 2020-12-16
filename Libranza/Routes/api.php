962307e5cd7a99d0472609eb06865757218a5f87
Daniel Alejandro Romero Morales <daniel.romero@laso.local>
2020-12-16 12:19:40 -0500

    api leads

--- Libranza/Routes/api.php
+++ Libranza/Routes/api.php
@@ -17,19 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::group(['prefix' => 'leads'], function () {
     Route::namespace('Api')->group(function () {
         Route::namespace('libranzaController')->group(function () {
-            Route::group(['middleware' => 'auth:api'], function () {
+            Route::group(['middleware' => ['api']], function () {
                 Route::post('register', 'LibranzaController@store');
             });
         });
     });
 });
 
-Route::group(['prefix' => 'leads/test'], function () {
-    Route::namespace('Api')->group(function () {
-        Route::namespace('libranzaController')->group(function () {
-            Route::group(['middleware' => 'auth:api'], function () {
-                Route::post('register', 'LibranzaController@testStore');
-            });
-        });
-    });
-});
