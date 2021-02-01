e9d1c9c6a782a61fa02037feac86526db3b1eaf6
Daniel Alejandro Romero Morales <daniel.romero@laso.local>
2021-02-01 14:48:48 -0500

    Campañas callCenter 01/01/2021

--- CallCenter/Entities/Campaigns/Imports/CampaignImport.php
+++ CallCenter/Entities/Campaigns/Imports/CampaignImport.php
@@ -4,8 +4,9 @@ namespace Modules\Courses\Entities\Campaigns\Imports;
 
 use Maatwebsite\Excel\Concerns\ToModel;
 use Modules\CallCenter\Entities\Campaigns\CallCenterCampaign;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
-class CampaignImport implements ToModel
+class CampaignImport implements ToModel, WithHeadingRow
 {
     public function model(array $row)
     {
