f484dfd5799e7194e2fb244c7d24cbfd3c7c6bf0
Daniel Alejandro Romero Morales <daniel.romero@laso.local>
2021-02-02 10:38:51 -0500

    Cargue

--- CallCenter/Entities/Campaigns/Imports/CampaignImport.php
+++ CallCenter/Entities/Campaigns/Imports/CampaignImport.php
@@ -3,14 +3,14 @@
 namespace Modules\Courses\Entities\Campaigns\Imports;
 
 use Maatwebsite\Excel\Concerns\ToModel;
-use Modules\CallCenter\Entities\Campaigns\CallCenterCampaign;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Modules\CallCenter\Entities\CampaignBases\CallCenterCampaignBase;
 
 class CampaignImport implements ToModel, WithHeadingRow
 {
     public function model(array $row)
     {
-        return new CallCenterCampaign([
+        return new CallCenterCampaignBase([
             'identity_number'        => $row['identity_number'],
             'employee_id'            => $row['employee_id'],   
             'campaign_id'            => $row['campaign_id'],          
